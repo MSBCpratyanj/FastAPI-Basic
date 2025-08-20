464658b140252ed13ce50786b6f25373445fc7bd
MSBCpratyanj <pratyanj.modh@msbcgroup.com>
2025-08-20 14:28:01 +0530

    created book model

---
+++ Books/models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String
+from database import base
+from sqlalchemy.orm import Mapped, mapped_column
+class Book(base):
+    __tablename__ = "books"
+
+    id:Mapped[int] = mapped_column(primary_key=True,index=True,autoincrement='auto')
+    title:Mapped[str] = mapped_column(nullable=False)
+    author:Mapped[str] = mapped_column(nullable=False)
+    description:Mapped[str] = mapped_column(nullable=True)
