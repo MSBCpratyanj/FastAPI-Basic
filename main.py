bf35b18b97597e7563dac92ca9bc159918708db0
MSBCpratyanj <pratyanj.modh@msbcgroup.com>
2025-08-20 14:31:14 +0530

    small change in main file

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 
 
 app = FastAPI()
 
 @app.get("/")
 async def index():
-    return {"message":"Index page API is running"}
+    return RedirectResponse(url="/docs")
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app,port=8080)
+    uvicorn.run("main:app",port=8080,reload=True)
