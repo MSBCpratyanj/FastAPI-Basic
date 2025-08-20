08d157d774127c455b4b83eca54b2eb47ed9673e
MSBCpratyanj <pratyanj.modh@msbcgroup.com>
2025-08-20 14:29:03 +0530

    Only use database connnection

--- database.py
+++ database.py
@@ -1,11 +1,14 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine,Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from uuid import UUID, uuid4
+from User.models import Gender
 
+DATABASE_NAME = "db.sqlite3"
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{DATABASE_NAME}"
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./db.sqlite3"
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL,connect_args={"check_same_thread":False})
+connect_args ={"check_same_thread":False} 
+engine = create_engine(SQLALCHEMY_DATABASE_URL,connect_args=connect_args)
 SessionLocal = sessionmaker(autoflush=False,expire_on_commit=False,bind=engine)
 
 base = declarative_base()
@@ -18,3 +21,11 @@ def get_db():
         print("DB connection fail")
     finally:
         db.close()
+
+class Book(base):
+    _tablename__ = "books"
+    
+    id = Column(Integer,primary_key=True,index=True)
+    title = Column(String, nullable=False)
+    author = Column(String, nullable=False)
+    description = Column(String, nullable=True)
