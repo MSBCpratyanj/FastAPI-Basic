3af55cf4d557cf342b4b170658c1af3fb3d42f2a
MSBCpratyanj <pratyanj.modh@msbcgroup.com>
2025-08-20 18:44:11 +0530

    bug fix on delete book API

--- Books/routers.py
+++ Books/routers.py
@@ -3,15 +3,20 @@ from fastapi.exceptions import HTTPException
 from sqlalchemy.orm import Session
 from . import models,schemas,crud
 from Database import database 
-
+import asyncio
 
 router = APIRouter(prefix="/Books",tags=["Books"])
 get_db = database.get_db
+
+async def send_mail(book_title:str):
+    await asyncio.sleep(3)
+    print(f"📧 Email sent: New book added → {book_title}")
 
 # create book
 @router.post('/',response_model=schemas.BookResponse,status_code=status.HTTP_201_CREATED)
 async def create_book(book:schemas.BookCreate,db:Session = Depends(get_db)):
     book = crud.create_book(db,book)
+    asyncio.create_task(send_mail(book.title))
     return book
 
 #Get all books 
@@ -33,9 +38,10 @@ async def update_book(book_id:int,book:schemas.BookCreate,db:Session = Depends(g
     return db_book
 
 # Delete book by ID
-@router.delete("/",status_code=status.HTTP_204_NO_CONTENT)
+@router.delete("/")
 async def del_book(book_id:int,db:Session = Depends(get_db)):
-    db_book = db.query(models.Book).filter(models.Book.id == book_id).first()
+    db_book = crud.delete_book(db,book_id)
     if not db_book:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"No Book available on ID({book_id})")
+    
     return {'Message':f"{db_book.title} book deleted successfully!"}
