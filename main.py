e4d94c3b3d2e2ca315c915392aa10210664659ba
MSBCpratyanj <pratyanj.modh@msbcgroup.com>
2025-08-20 18:07:47 +0530

    add book router and create table

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
+from Books.routers import  router as books_router
+from  sqlalchemy.schema import MetaData
+from Database.database import base,engine
 
+from Books import models
 
+models.base.metadata.create_all(bind=engine)
 app = FastAPI()
+
+app.include_router(books_router)
 
 @app.get("/")
 async def index():
