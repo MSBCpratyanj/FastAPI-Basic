cb60d28cca7c677e5e80d2c87a9e6d7730c044bf
MSBCpratyanj <pratyanj.modh@msbcgroup.com>
2025-08-21 15:58:16 +0530

    Add CORS and new router

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
-from Books.routers import  router as books_router
 from  sqlalchemy.schema import MetaData
 from Database.database import base,engine
+from starlette.middleware.cors import CORSMiddleware
 
-from Books import models
+from Books.routers import  router as books_router
+from auth.router import auth_router
+from User.router import user_router
 
-models.base.metadata.create_all(bind=engine)
-app = FastAPI()
+base.metadata.create_all(bind=engine)
+openapi_tags = [
+    {
+        "name": "Users",
+        "description": "User operations",
+    },
+    {
+        "name": "Books",
+        "description":"Book operations",
+    }
+]
 
+app = FastAPI(openapi_tags=openapi_tags)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(auth_router, prefix='/api')
+app.include_router(user_router, prefix='/api', tags=['Users'])
 app.include_router(books_router)
 
 @app.get("/")
